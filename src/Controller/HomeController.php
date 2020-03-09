aeef5cfac76c29e56cda84a3afb693f0d16f6b52
Maxime <57092080+Elvih@users.noreply.github.com>
2020-03-09 13:10:41 +0100

    Update HomeController.php
    Mise à jour Route/Annotation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,25 +1,20 @@
 <?php
 namespace App\Controller;
 
-use Twig\Environment;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeController
+class HomeController extends AbstractController
 {
+
     /**
-     * @var Environment
+     * @Route("/", name="home")
+     * @return Response
      */
-    private $twig;
-
-
-    public function __construct(Environment $twig)
-    {
-        $this->twig = $twig;
-    }
-
     public function index(): Response
     {
-        return new Response($this->twig->render('pages/home.html.twig'));
+        return $this->render('pages/home.html.twig');
     }
 
 }
